772632de64be77ca268857c0e2638426b751b391
hashimMH <hashimstar25@gmail.com>
2025-04-08 17:35:40 +0400

    adding psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x,y):
+	"""add x and y and return result"""
+	return x + y
+
+def subtract(x, y):
+	"""substracting y from x and return result"""
+	return y - x
